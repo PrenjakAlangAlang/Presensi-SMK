1fd0ee701f52fbb5e79025dd6cfd0e98ee19fa84
PrenjakAlangAlang <luthfinurafiq76@gmail.com>
2026-06-17 20:32:02 +0700

    Update generation time in presensi_smk migration file

--- db/migrations/presensi_smk (13).sql
+++ db/migrations/presensi_smk (13).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 04, 2026 at 03:20 AM
+-- Generation Time: Jun 17, 2026 at 01:30 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
